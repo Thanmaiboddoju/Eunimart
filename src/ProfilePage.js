bafec02530bd3973df6db4ea375c93d1106ac158
Thanmaiboddoju <89909723+Thanmaiboddoju@users.noreply.github.com>
2022-05-27 11:18:29 +0530

    Add files via upload

---
+++ src/ProfilePage.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import {useEffect,useState} from 'react';
+import './SocialCard.css';
+import Profile from './Profile';
+import SocialCard from './SocialCard';
+
+function ProfilePage(){
+	const [profiles,setProfiles]=useState([]);
+	useEffect(() => {
+    (async () => {
+      let userData;
+      try {
+        const response = await fetch("https://reqres.in/api/users");
+        userData = (await response.json()).data;
+      } catch (error) {
+        console.log(error);
+        userData = [];
+      }
+      setProfiles(userData);
+ })();
+  }, []);
+	
+	return(
+		<div className="ProfilePage">
+      <div className="cards-container">
+      {profiles.map((user, index) => (
+        <Profile key={index} userData={user} />
+        ))}
+        </div>
+      </div>
+	)
+		
+}
+export default ProfilePage;
